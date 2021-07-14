55f2d474cc5eb7fce8e4dd301ca27cfb30185029
Daniel Nottingham <danielnottingham.ti@gmail.com>
2021-07-14 15:38:38 -0300

    g users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
